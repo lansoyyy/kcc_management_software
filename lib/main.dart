7ccba91602816399bf82a27dd0af62370a4c0676
Lans12345 <olanalans12345@gmail.com>
2023-07-27 16:11:56 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kcc_management_software/screens/playerlist_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          authDomain: 'kcc-membership-software.firebaseapp.com',
+          apiKey: "AIzaSyC8_H46SpFo-k71y_4PIcbhHyFBIbgqyOA",
+          appId: "1:998351460963:web:352c1a8627c117308d7656",
+          messagingSenderId: "998351460963",
+          projectId: "kcc-membership-software",
+          storageBucket: "kcc-membership-software.appspot.com"));
   runApp(const MyApp());
 }
 
