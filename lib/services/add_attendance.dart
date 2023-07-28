9ac192b2d97f1f0d3d8f8fd96e3e89f17ca638ab
Lans12345 <olanalans12345@gmail.com>
2023-07-28 17:28:22 +0800

    added attendance service

---
+++ lib/services/add_attendance.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future addAttendance(String firstName, String lastName, String middleInitial,
+    String userId, String photo, bool isActive) async {
+  final docUser = FirebaseFirestore.instance.collection('Attendance').doc();
+
+  final json = {
+    'firstName': firstName,
+    'lastName': lastName,
+    'middleInitial': middleInitial,
+    'id': docUser.id,
+    'dateTime': DateTime.now(),
+    'userId': userId,
+    'photo': photo,
+    'isActive': isActive,
+    'day': DateTime.now().day,
+    'month': DateTime.now().month,
+    'year': DateTime.now().year,
+  };
+
+  await docUser.set(json);
+}
